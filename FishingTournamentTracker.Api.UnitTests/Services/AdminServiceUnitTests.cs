92be9102e9ea2824d1a042690a08aac8533f0e37
robert baxter <baxterrp@gmail.com>
2024-07-07 17:17:15 -0400

    started services tests

---
+++ FishingTournamentTracker.Api.UnitTests/Services/AdminServiceUnitTests.cs
@@ -0,0 +1,56 @@
+﻿using FishingTournamentTracker.Api.Exeptions;
+using FishingTournamentTracker.Api.Repositories;
+using FishingTournamentTracker.Api.Services;
+using FishingTournamentTracker.Library.Models.DataModels;
+using FluentAssertions;
+using Moq;
+
+namespace FishingTournamentTracker.Api.UnitTests.Services;
+
+public class AdminServiceUnitTests
+{
+    private readonly Mock<IAdminRepository> _mockAdminRepository;
+    private readonly IAdminService _adminService;
+
+    public AdminServiceUnitTests()
+    {
+        _mockAdminRepository = new Mock<IAdminRepository>();
+        _adminService = new AdminService(_mockAdminRepository.Object);
+    }
+
+    [Fact]
+    public async Task Login_WhenAdminIsNull_ThrowsArgumentNullException()
+    {
+        // Arrange/Act/Assert
+        var controllerAction = async () => await _adminService.Login(null!);
+        await controllerAction.Should().ThrowAsync<ArgumentNullException>()
+            .WithMessage("Value cannot be null. (Parameter 'admin')")
+            .WithParameterName("admin");
+    }
+
+    [Fact]
+    public async Task Login_WhenAdminUserNameIsNull_ThrowsArgumentException()
+    {
+        // Arrange/Act/Assert
+        var controllerAction = async () => await _adminService.Login(new Admin());
+        await controllerAction.Should().ThrowAsync<ArgumentException>()
+            .WithParameterName(nameof(Admin.UserName));
+    }
+
+    [Fact]
+    public async Task Login_WhenRepositoryCallReturnsNull_ThrowsUnauthorizedLoginAttemptException()
+    {
+        // Arrange
+        var admin = new Admin
+        {
+            UserName = Guid.NewGuid().ToString(),
+            Password = Guid.NewGuid().ToString(),
+        };
+
+        _mockAdminRepository.Setup(repo => repo.Login(admin)).ReturnsAsync((Admin?)null);
+
+        // Act/Assert
+        var controllerAction = async () => await _adminService.Login(admin);
+        await controllerAction.Should().ThrowAsync<UnauthorizedLoginAttemptException>();
+    }
+}
---
+++ FishingTournamentTracker.Api.UnitTests/Services/FileParserUnitTests.cs
@@ -0,0 +1,76 @@
+﻿using FishingTournamentTracker.Api.Services;
+using FishingTournamentTracker.Library.Models.DataModels;
+using FluentAssertions;
+
+namespace FishingTournamentTracker.Api.UnitTests.Services;
+
+public class FileParserUnitTests
+{
+    private readonly IFileParser _fileParser;
+
+    public FileParserUnitTests()
+    {
+        _fileParser = new FileParser();
+    }
+
+    [Fact]
+    public void GenerateExcel_WithValidInput_ReturnsNonEmptyByteArray()
+    {
+        // Arrange
+        var entities = GetTestEntities();
+
+        // Act
+        var result = _fileParser.GenerateExcel(entities);
+
+        // Assert
+        result.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public void ParseExcel_WithValidInput_ReturnsListOfEntities()
+    {
+        // Arrange
+        var expectedEntities = GetTestEntities();
+        var entityBytes = _fileParser.GenerateExcel(expectedEntities);
+
+        // Act
+        var result = _fileParser.ParseExcel<TestEntity>(entityBytes);
+
+        // Assert
+        // Id does not get written on file upload
+        result.Should().BeEquivalentTo(expectedEntities, options => options.Excluding(entity => entity.Id));
+    }
+
+    private static List<TestEntity> GetTestEntities()
+    {
+        return
+        [
+            new TestEntity
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = Guid.NewGuid().ToString()
+            },
+            new TestEntity
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = Guid.NewGuid().ToString()
+            },
+            new TestEntity
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = Guid.NewGuid().ToString()
+            },
+            new TestEntity
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = Guid.NewGuid().ToString()
+            }
+        ];
+    }
+
+    internal class TestEntity : IDatabaseEntity
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
